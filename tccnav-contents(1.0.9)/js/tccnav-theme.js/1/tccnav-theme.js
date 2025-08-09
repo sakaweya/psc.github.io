2f9f1e1b2b7f081e1cb09ffe20e922af851e4646
sakaweya <kawadanobuhiro@gmail.com>
2025-08-09 11:31:46 +0900

    aa
    ss

--- tccnav-contents(1.0.9)/js/tccnav-theme.js/1/tccnav-theme.js
+++
@@ -1,5 +0,0 @@
-$(document).on('mobileinit', function () {
-    $.mobile.page.prototype.options.theme = 'c';
-    $.mobile.listview.prototype.options.theme = 'd';
-    $.mobile.listview.prototype.options.dividerTheme = 'c';
-});
